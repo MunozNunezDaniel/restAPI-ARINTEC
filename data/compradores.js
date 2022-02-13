450f3430982cda562689249d18cd7abdc9c84811
daniel <danielmznz5@gmail.com>
2022-02-13 22:48:50 +0100

    Funciona

--- data/compradores.js
+++ data/compradores.js
@@ -1,21 +1,21 @@
 db.compradores.insertMany([
     {
-        "_nombre_comprador": "Daniel Muñoz Rodriguez",
+        "_nombre_comprador": "Daniel_Muñoz_Rodriguez",
         "_presupuesto": 20000,
         "_n_telefono": 686993301
     },
     {
-        "_nombre_comprador": "Juan Martin Jimenez",
+        "_nombre_comprador": "Juan_Martin_Jimenez",
         "_presupuesto": 40000,
         "_n_telefono": 234009988
     },
     {
-        "_nombre_comprador": "Damaris Mercado Suyo",
+        "_nombre_comprador": "Damaris_Mercado_Suyo",
         "_presupuesto": 6000,
         "_n_telefono": 787334499
     },
     {
-        "_nombre_comprador": "Rafa Perez Nuñez",
+        "_nombre_comprador": "Rafa_Perez_Nuñez",
         "_presupuesto": 25000,
         "_n_telefono": 756990033
     }
