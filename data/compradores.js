c2b9470ff3ebdf1d0651fb2fee65f7e5d08eec75
daniel <danielmznz5@gmail.com>
2022-02-13 22:40:10 +0100

    Quito identif

--- data/compradores.js
+++ data/compradores.js
@@ -1,24 +1,20 @@
 db.compradores.insertMany([
     {
-        "_identif": "tg536",
         "_nombre_comprador": "Daniel Muñoz Rodriguez",
         "_presupuesto": 20000,
         "_n_telefono": 686993301
     },
     {
-        "_identif": "fr324",
         "_nombre_comprador": "Juan Martin Jimenez",
         "_presupuesto": 40000,
         "_n_telefono": 234009988
     },
     {
-        "_identif": "gh567",
         "_nombre_comprador": "Damaris Mercado Suyo",
         "_presupuesto": 6000,
         "_n_telefono": 787334499
     },
     {
-        "_identif": "vb564",
         "_nombre_comprador": "Rafa Perez Nuñez",
         "_presupuesto": 25000,
         "_n_telefono": 756990033
